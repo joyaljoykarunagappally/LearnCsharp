17952d677b1747c5566e232ccb1215dd525e41dd
joyal <joyal@DEVELOPER>
2024-04-26 12:07:17 +0530

    Add project files.

---
+++ LearnCsharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LearnCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Ch = "john";
+            Console.WriteLine("Hello " + Ch);
+            Ch = "manu";
+            Console.WriteLine("hello" + Ch);
+        }
+    }
+}
